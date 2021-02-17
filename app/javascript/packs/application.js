ae1aad0eee1b92ae9e1571484654a1ecf42477b3
Avalon van der Horst <avalonvdhorst@gmail.com>
2021-02-17 11:33:49 +0100

    add params disabled to bicylemodel add styling to new disabled options form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -18,18 +17,19 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
-
 // External imports
 import "bootstrap";
+import {initTomSelect} from "./tom_select_init"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initTomSelect();
 });
+
+// const doButton = document.querySelector(".newDo");
+// doButton.addEventListener("click", () => {
+//   initTomSelect();
+// })
--- app/models/disabled_option.rb
+++ app/models/disabled_option.rb
@@ -1,4 +1,5 @@
 class DisabledOption < ApplicationRecord
   belongs_to :option, foreign_key: :option_one_id
+  belongs_to :option, foreign_key: :option_two_id
   # belongs_to :option_type, through: :option
 end
