d2b679ab090b26fc5cfbe48afc7bacc2495f3a7e
Avalon van der Horst <avalonvdhorst@gmail.com>
2021-02-22 10:39:34 +0100

    update active storage heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_111733) do
+ActiveRecord::Schema.define(version: 2021_02_22_093049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
