e3797aaae2fa4adeae57399147dcda985ce58300
Avalon van der Horst <avalonvdhorst@gmail.com>
2021-02-22 17:03:28 +0100

    small change in select disabled option

--- app/javascript/packs/init_disabled_options.js
+++ app/javascript/packs/init_disabled_options.js
@@ -1,10 +1,10 @@
 const initDisabledOptions = () => {
-  const dopForm = document.querySelector(".card-grid")
+  const dopForm = document.querySelector("form#new_bicycle")
   if (dopForm) {
     const pills = dopForm.querySelectorAll(".pill-container input");
+    console.log(pills)
     pills.forEach((pill) => {
       const dopIds = JSON.parse(pill.dataset.disabledIds)
-      console.log(dopIds)
       pill.addEventListener("click", () => {
         dopIds.forEach((dop) => {
           if (pill.checked)  {
